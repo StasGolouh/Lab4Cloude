59070935700a7ecf85cecccf61d4dde297f48edb
Stanislav Golouh <134232026+StasGolouh@users.noreply.github.com>
2025-10-16 20:44:40 +0300

    Fix port assignment with correct fallback

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const url = require('url');
 
-const port = process.env.PORT  3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
