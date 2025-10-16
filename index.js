082e63c4541ec0216118c99b3e8e4d21fd2eec9e
Stanislav Golouh <134232026+StasGolouh@users.noreply.github.com>
2025-10-16 20:40:44 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const url = require('url');
+
+const port = process.env.PORT  3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
+    const parsedUrl = url.parse(req.url, true);
+    const name = parsedUrl.query.name  'world';
+
+    const responseObject = {
+        hello: name,
+        runtime: 'nodejs',
+        region: process.env.GCP_REGION || 'unknown'
+    };
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(responseObject));
+});
+
+server.listen(port, () => {
+    console.log(Server running on port ${port});
+});
