2fcf7ef339bc61a2857869a4fbf15e366835ef9f
Stanislav Golouh <134232026+StasGolouh@users.noreply.github.com>
2025-10-17 13:20:49 +0300

    Change server port from 3000 to 1111

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const url = require('url');
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1111;
 
 const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,16 +16,16 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const name = parsedUrl.query.name || 'world';
 
-    const responseObject = {
+    const responseObj = {
         hello: name,
         runtime: 'nodejs',
         region: process.env.GCP_REGION || 'unknown'
     };
     
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(responseObject));
+    res.end(JSON.stringify(responseObj));
 });
 
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server's running on port ${port}`);
 });
